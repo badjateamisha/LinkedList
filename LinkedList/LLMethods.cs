be1c470c70a38dcdc67c670ab8bd6ca1c4cc4c68
badjateamisha <amishabadjate175@gmail.com>
2022-05-12 14:56:15 +0530

    Simple Linked List

---
+++ LinkedList/LLMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LLMethods<T>
+    {
+        public int count = 0;
+        Node<T> head;
+
+
+        public void AddNodeToFront(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            node.next = head;
+            head = node;
+            count++;
+        }
+
+        public void display()
+        {
+            Node<T> temp = head;
+            while (temp != null)
+            {
+                Console.Write("->{0}", temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinkedList;
+
+
+Console.WriteLine("Types of Linked List Methods!");
+
+while (true)
+{
+    Console.WriteLine("Select the Program to be Executed :\n1)Add from Start");
+    int Option = Convert.ToInt32(Console.ReadLine());
+    switch (Option)
+    {
+        case 1:
+            LLMethods<int> lLMethods = new();
+            lLMethods.AddNodeToFront(76);
+            lLMethods.AddNodeToFront(30);
+            lLMethods.AddNodeToFront(56);
+
+            Console.WriteLine("The Elements of the Linkedlist are : ");
+            lLMethods.display();
+            Console.WriteLine("\n");
+
+            break;
+    }
+}
+
