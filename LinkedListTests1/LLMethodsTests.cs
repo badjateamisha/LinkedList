1a12f623b607b5dc23092d75b87492e35628c0a8
badjateamisha <amishabadjate175@gmail.com>
2022-05-12 16:23:09 +0530

    Searching for an element

---
+++ LinkedListTests1/LLMethodsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList.Tests
+{
+    [TestClass()]
+    public class LLMethodsTests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            LLMethods<int> lLMethods = new();
+            lLMethods.AddNodeToFront(70);
+            lLMethods.AddNodeToFront(30);
+            lLMethods.AddNodeToFront(56);
+            lLMethods.SearchMethod(30);
+            Assert.AreEqual(true, lLMethods.SearchMethod(30));
+
+        }
+    }
+}
